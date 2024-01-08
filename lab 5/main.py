83d3f2c3e2bd9e9791bc58a855d8ed09fee6f3a6
miruuunaa <149154499+miruuunaa@users.noreply.github.com>
2024-01-08 21:27:43 +0200

    Update main.py

--- lab 5/main.py
+++ lab 5/main.py
@@ -3,10 +3,10 @@ from modele.drink import Drink
 from modele.cooked_dish import Cooked_Dish
 
 def main():
-    dishes1 = {'dish1': 12, 'dish2': 30}
-    drinks1 = {'drink1': 6, 'drink2': 5}
-    dishes2 = {'dish1': 56, 'dish2': 90}
-    drinks2 = {'drink1': 45, 'drink2': 15}
+    dishes1 = {'dish1': 13, 'dish2': 35}
+    drinks1 = {'drink1': 7, 'drink2': 6}
+    dishes2 = {'dish1': 57, 'dish2': 95}
+    drinks2 = {'drink1': 46, 'drink2': 16}
 
     order1 = Order('order1', 'bob', dishes1, drinks1)
     order2 = Order('order2', 'dob', dishes2, drinks2)
